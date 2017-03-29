b3e9d865f1f41205fd0f98075c807f40dd30a2bf
Sergey <elenergetica87@mail.ru>
2017-03-29 21:18:33 +0300

    add two section

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,7 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/styles');
 
-gulp.task('default', function(){
-    console.log("Horray!");
-});
 
-gulp.task('html', function(){
-    console.log("Horray!123123123");
-});
 
-gulp.task('watch', function(){
-    
-    watch('index.html', function(){
-        gulp.start('html');
-    });
-});
+
+
